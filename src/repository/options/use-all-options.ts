8f5ffd64fa7bc79e195a059bb2486788001158f8
Ravin <rvnda.putra@gmail.com>
2022-12-08 17:10:54 +0700

    feat: add repository for all options

---
+++ src/repository/options/use-all-options.ts
@@ -0,0 +1,48 @@
+import { useMemo } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+import { EFISHERY_API } from "../../model";
+import type {
+  OptionAreaQueryResponse,
+  OptionSizeQueryResponse,
+} from "../../model/options";
+import { normalizeOptArea, normalizeOptSize } from "./normalizer";
+
+const useAllOptions = () => {
+  const {
+    data = [],
+    error,
+    isLoading,
+  } = useQuery<[OptionAreaQueryResponse[], OptionSizeQueryResponse[]]>({
+    queryKey: ["all_options"],
+    queryFn: async ({ signal }) => {
+      const respArea = await fetch(EFISHERY_API.options.area, { signal });
+      const respSize = await fetch(EFISHERY_API.options.size, { signal });
+
+      const area = await respArea.json();
+      const size = await respSize.json();
+
+      if (!respArea.ok || !respSize.ok) {
+        throw new Error("Network response was not oke");
+      }
+
+      return Promise.all([area, size]);
+    },
+  });
+
+  return useMemo(() => {
+    const normalizedOptArea = normalizeOptArea(data[0]);
+    const normalizedOptSize = normalizeOptSize(data[1]);
+
+    return {
+      data: {
+        area: normalizedOptArea,
+        size: normalizedOptSize,
+      },
+      error: error,
+      loading: isLoading,
+    };
+  }, [data, error, isLoading]);
+};
+
+export default useAllOptions;

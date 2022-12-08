2651a2ebc653895644f65f9d9f70a91ec49db71d
Ravin <rvnda.putra@gmail.com>
2022-12-08 13:31:58 +0700

    chore: initialize and add react-query, add eFishery API collections

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import routes from "./routes";
 
+const queryClient = new QueryClient();
 const router = createBrowserRouter(routes);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
---
+++ src/model/constants.ts
@@ -0,0 +1,18 @@
+/**
+ * @constants STEIN_EFISHERY_URI
+ * TODO: create a .env file and move this into .env file
+ */
+export const STEIN_EFISHERY_URI =
+  "https://stein.efishery.com/v1/storages/5e1edf521073e315924ceab4";
+
+/**
+ * @constant EFISHERY_API
+ * @description eFishery API collections
+ */
+export const EFISHERY_API = {
+  options: {
+    area: `${STEIN_EFISHERY_URI}/option_area`,
+    size: `${STEIN_EFISHERY_URI}/option_size`,
+  },
+  list: `${STEIN_EFISHERY_URI}/list`,
+};
